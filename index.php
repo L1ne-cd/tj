38c9fdc9d9cbcd3dfc21fce1e53a2b9c4aa35d0c
L1ne-cd <l1nve@outlook.com>
2024-11-30 22:56:04 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>从 0 开始的PHP反序列化引导靶场</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-md py-3">
+        <div class="container">
+            <a class="navbar-brand d-flex align-items-center" href="#"><img src="assets/img/tj.svg"><span>&nbsp;探姬</span></a>
+        </div>
+    </nav>
+    <h1>从 0 开始的PHP反序列化入门靶场</h1>
+    <div class="levels">
+        <a class="level-link" href="Level1/index.php">Level 1: </a>
+        <a class="level-link" href="Level2/index.php">Level 2: </a>
+        <a class="level-link" href="Level3/index.php">Level 3: </a>
+        <a class="level-link" href="Level4/index.php">Level 4: </a>
+        <a class="level-link" href="Level5/index.php">Level 5: </a>
+        <a class="level-link" href="Level6/index.php">Level 6: </a>
+        <a class="level-link" href="Level7/index.php">Level 7: </a>
+        <a class="level-link" href="Level8/index.php">Level 8: </a>
+        <a class="level-link" href="Level9/index.php">Level 9: </a>
+        <a class="level-link" href="Level10/index.php">Level 10: </a>
+        <a class="level-link" href="Level11/index.php">Level 11: </a>
+        <a class="level-link" href="Level12/index.php">Level 12: </a>
+        <a class="level-link" href="Level13/index.php">Level 13: </a>
+        <a class="level-link" href="Level14/index.php">Level 14: </a>
+        <a class="level-link" href="Level15/index.php">Level 15: </a>
+        <a class="level-link" href="Level16/index.php">Level 16: </a>
+        <a class="level-link" href="Level17/index.php">Level 17: </a>
+        <a class="level-link" href="Level18/index.php">Level 18: </a>
+        <a class="level-link" href="Level18/index.php">Level 19: </a>
+        <a class="level-link" href="Level18/index.php">Level 20: </a>
+        <a class="level-link" href="Level18/index.php">Level 21: </a>
+        <a class="level-link" href="Level18/index.php">Level 22: </a>
+        <a class="level-link" href="Level18/index.php">Level 23: </a>
+        <a class="level-link" href="Level18/index.php">Level 24: </a>
+        <a class="level-link" href="Level18/index.php">Level 25: </a>
+        <a class="level-link" href="Level18/index.php">Level 26: </a>
+        <a class="level-link" href="Level18/index.php">Level 27: </a>
+
+    </div>
+    <div class="footer">
+        <p>© 2024 Probius | <a href="https://github.com/ProbiusOfficial/PHPSerialize-labs">GitHub</a></p>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
